c23eca7775d599403dd12cb595d384fcf19fbe76
Sanket Singh <singhsanket143@gmail.com>
2023-09-07 19:37:44 +0530

    Added home page ui

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,32 @@
-import Footer from "Components/Footer/Footer";
+import Logo from 'Assets/Images/logo-color.png';
 
 export default function Home() {
     return (
-        <>Home component <Footer /> </>
+            <div className="flex flex-col items-center justify-center gap-20 h-[100vh]">
+
+                <div className="h-48 w-48">
+                    <img 
+                        className='w-full h-full'
+                        alt="logo"
+                        src={Logo}
+                    />
+                </div>
+
+                <div className='flex justify-around items-center gap-16'>
+                    <div className='w-2/4 text-center font-semibold basis-1/2'>
+                        <h1 className='text-white text-5xl tracking-widest leading-normal'>
+                            BookShelf <br/>
+                            <span className='text-warning'>
+                                Your personal library and social network for bookworms
+                            </span>
+                        </h1>
+                    </div>
+                    <div>
+                        <button className='btn btn-primary rounded-md px-5 py-2 text-xl'>Register</button>
+                        <button className='btn btn-warning mx-3 rounded-md px-5 py-2 text-xl'>Login</button>
+                    </div>
+                </div>
+
+            </div>
     );
 }
